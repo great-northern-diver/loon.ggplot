b465ac8c7cedc56b7bc9eed1a4b52b61b6418c7d
z267xu <40337165+z267xu@users.noreply.github.com>
2018-07-03 11:37:21 -0400

    add more groups

--- R/polarGuide.R
+++ R/polarGuide.R
@@ -1,6 +1,5 @@
 
-
-coordPolarGuides <- function(widget, ggplotPanel_params, theta){
+polarGuides <- function(widget, ggplotPanel_params, swapAxes){
 
   theta.range <- ggplotPanel_params$theta.range
   r.range <- ggplotPanel_params$r.range
@@ -27,61 +26,74 @@ coordPolarGuides <- function(widget, ggplotPanel_params, theta){
   angles <- 2 * pi * (theta.minor - theta.range[1])/diff(theta.range)
   textAngles <- 2 * pi * (theta.major - theta.range[1])/diff(theta.range)
   # set group
-  guidesGroup <- l_layer_group(widget, "polar guides")
+  guidesGroup <- l_layer_group(widget, "Polar Guides")
   # draw grey background
   extendRange <- grDevices::extendrange(c(-maxRadius, maxRadius))
-  rectLayer <- l_layer_rectangle(widget, x = extendRange, y = extendRange,
-                                 color = "#EBEBEBEBEBEB", linecolor = "", parent = guidesGroup)
-  if(theta == "x"){
+  rectLayer <- l_layer_rectangle(widget,
+                                 x = extendRange,
+                                 y = extendRange,
+                                 color = widget['guidesBackground'],
+                                 linecolor = "",
+                                 parent = guidesGroup)
+  if (swapAxes) {
     # draw radius label
+    labelsGroup <- l_layer_group(widget, "radius labels", parent = guidesGroup)
     textRadiiLayer <- lapply(1:length(textRadius), function(i){
-      l_layer_text(widget, x = extendRange[1], y = textRadius[i], color = "black", parent = guidesGroup,
-                   text = r.labels[i], size = 9)
+      l_layer_text(widget, x = textRadius[i], y = extendRange[1], color = widget['foreground'], parent = labelsGroup,
+                   text = r.labels[i], size = 9, anchor = "e")
     })
     # draw ovals
-    ovalsLayer <- lapply(radius, function(l){
-      xleft <- ybottom <- -l
-      xright <- ytop <- l
-      l_layer_oval(widget, x = c(xleft, xright), y = c(ybottom, ytop),
-                   color = "", linecolor = "white", parent = guidesGroup)
-    })
-    # draw lines
-    linesLayer <- lapply(angles, function(l){
-      xend <- maxRadius * sin(l)
-      yend <- maxRadius * cos(l)
-      l_layer_line(widget, x = c(0, xend), y = c(0, yend),
-                   color = "white", parent = guidesGroup)
-    })
-    # draw lines label
-    linesLabelLayer <- lapply(1:length(textAngles), function(i){
-      l_layer_text(widget, x = maxRadius * sin(textAngles[i]), y = maxRadius * cos(textAngles[i]),
-                   color = "black", parent = guidesGroup, text = theta.labels[i], size = 9)
-    })
-  } else if(theta == "y") {
-    textRadiiLayer <- lapply(1:length(textRadius), function(i){
-      l_layer_text(widget, y = extendRange[1], x = textRadius[i], color = "black", parent = guidesGroup,
-                   text = r.labels[i], size = 9)
-    })
-    # draw ovals
+    ovalGroup <- l_layer_group(widget, "circle guides", parent = guidesGroup)
     ovalsLayer <- lapply(radius, function(l){
       xleft <- ybottom <- -l
       xright <- ytop <- l
       l_layer_oval(widget, y = c(xleft, xright), x = c(ybottom, ytop),
-                   color = "", linecolor = "white", parent = guidesGroup)
+                   color = "", linecolor = widget['guidelines'], parent = ovalGroup)
     })
     # draw lines
+    linesGroup <- l_layer_group(widget, "radius guides", parent = guidesGroup)
     linesLayer <- lapply(angles, function(l){
       xend <- maxRadius * sin(l)
       yend <- maxRadius * cos(l)
       l_layer_line(widget, y = c(0, xend), x = c(0, yend),
-                   color = "white", parent = guidesGroup)
+                   color = widget['guidelines'], parent = linesGroup)
     })
     # draw lines label
+    line_labelsGroup <- l_layer_group(widget, "line labels", parent = guidesGroup)
     linesLabelLayer <- lapply(1:length(textAngles), function(i){
       l_layer_text(widget, y = maxRadius * sin(textAngles[i]), x = maxRadius * cos(textAngles[i]),
-                   color = "black", parent = guidesGroup, text = theta.labels[i], size = 9)
+                   color = widget['foreground'], parent = line_labelsGroup, text = theta.labels[i], size = 9)
     })
-  } else NULL
+  } else {
+    # draw radius label
+    labelsGroup <- l_layer_group(widget, "radius labels", parent = guidesGroup)
+    textRadiiLayer <- lapply(1:length(textRadius), function(i){
+      l_layer_text(widget, x = extendRange[1], y = textRadius[i], color = widget['foreground'], parent = labelsGroup,
+                   text = r.labels[i], size = 9, anchor = "e")
+    })
+    # draw ovals
+    ovalGroup <- l_layer_group(widget, "circle guides", parent = guidesGroup)
+    ovalsLayer <- lapply(radius, function(l){
+      xleft <- ybottom <- -l
+      xright <- ytop <- l
+      l_layer_oval(widget, x = c(xleft, xright), y = c(ybottom, ytop),
+                   color = "", linecolor = widget['guidelines'], parent = ovalGroup)
+    })
+    # draw lines
+    linesGroup <- l_layer_group(widget, "radius guides", parent = guidesGroup)
+    linesLayer <- lapply(angles, function(l){
+      xend <- maxRadius * sin(l)
+      yend <- maxRadius * cos(l)
+      l_layer_line(widget, x = c(0, xend), y = c(0, yend),
+                   color = widget['guidelines'], parent = linesGroup)
+    })
+    # draw lines label
+    line_labelsGroup <- l_layer_group(widget, "line labels", parent = guidesGroup)
+    linesLabelLayer <- lapply(1:length(textAngles), function(i){
+      l_layer_text(widget, x = maxRadius * sin(textAngles[i]), y = maxRadius * cos(textAngles[i]),
+                   color = widget['foreground'], parent = line_labelsGroup, text = theta.labels[i], size = 9)
+    })
+  }
 
   guidesGroup
 }
