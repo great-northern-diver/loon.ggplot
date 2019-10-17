24b800c789fc076589ef5ee6a983569710a8fe7c
z267xu <z267xu@github.com>
2019-10-16 21:12:07 -0400

    line up code

--- R/loon2ggplot_l_serialaxes.R
+++ R/loon2ggplot_l_serialaxes.R
@@ -13,20 +13,20 @@ loon2ggplot.l_serialaxes <- function(target, ...) {
 
   ggObj <- ggplot2::ggplot(data = data) %>%
     ggSerialAxes(scaling = widget['scaling'],
-                    layout = widget['axesLayout'],
-                    color = get_display_color(
-                      as_hex6color(widget['color'][active_displayOrder]),
-                      widget['selected'][active_displayOrder]
-                    ),
-                    size = as_r_line_size(widget['linewidth'][active_displayOrder]),
-                    axesLabels = widget['sequence'],
-                    title = widget['title'],
-                    showLabels =  widget['showLabels'],
-                    showAxesLabels = widget['showAxesLabels'],
-                    showGuides = widget['showGuides'],
-                    showAxes = widget['showAxes'],
-                    displayOrder = active_displayOrder,
-                    showArea = widget['showArea'])
+                 layout = widget['axesLayout'],
+                 color = get_display_color(
+                   as_hex6color(widget['color'][active_displayOrder]),
+                   widget['selected'][active_displayOrder]
+                 ),
+                 size = as_r_line_size(widget['linewidth'][active_displayOrder]),
+                 axesLabels = widget['sequence'],
+                 title = widget['title'],
+                 showLabels =  widget['showLabels'],
+                 showAxesLabels = widget['showAxesLabels'],
+                 showGuides = widget['showGuides'],
+                 showAxes = widget['showAxes'],
+                 displayOrder = active_displayOrder,
+                 showArea = widget['showArea'])
 
   return(ggObj)
 }
