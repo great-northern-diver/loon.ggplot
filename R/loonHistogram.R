d73bd114dfd8fe81f0797d416cc8199278b77a20
z267xu <z267xu@github.com>
2019-11-27 11:45:13 -0500

    position_dodge can be displayed

--- R/loonHistogram.R
+++ R/loonHistogram.R
@@ -131,8 +131,8 @@ loonHistogram.StatCount <- function(ggBuild, ggLayout, layout, ggplotPanel_param
                title = loonTitle)
 }
 
-# catch_bin_info is mainly used to catch the order of the bins
-# since we users set ylim or xlim the bins would change
+# catch_bin_info is mainly used to catch the order of the stacked bins
+# when users set ylim or xlim, the bins would change.
 catch_bin_info <- function(hist_values, hist_data, x.limits, y.limits,
                            facet_id, in_limits, dataFrame,
                            ggObj, column_names, activeGeomLayers, ...) {
@@ -195,15 +195,15 @@ catch_bin_info.StatBin <- function(hist_values, hist_data, x.limits, y.limits,
   fill <- rep(uni_fill, length(hist_values))
   # set stack color
   if (!is.null(ggObj$labels$fill)) {
+    # fill: panel i && in limits
     fill_var <- unlist(
       dataFrame[, which(stringr::str_detect(ggObj$labels$fill, column_names) == TRUE)]
-    )
-    panel_i.fill_var <- fill_var[facet_id][in_limits]
+    )[facet_id][in_limits]
     levels <- rev(levels(as.factor(fill_var)))
     if (length(uni_fill) == length(levels)) {
       fill <- rep(NA, length(hist_values))
       for(j in seq_len(length(levels))){
-        fill[which(panel_i.fill_var %in% levels[j])] <- uni_fill[j]
+        fill[which(fill_var %in% levels[j])] <- uni_fill[j]
       }
     }
   }
@@ -229,15 +229,23 @@ catch_bin_info.StatCount <- function(hist_values, hist_data, x.limits, y.limits,
     in_x.limits <- hist_values %in% x.limits
   }
 
-  if(!is.null(ggObj$labels$fill) && ggObj$labels$fill != ggObj$labels$x) {
+  position <- ggObj$layers[[activeGeomLayers]]$position
+  reverse <- position$reverse
+  operations <- position_operation(position,
+                                   hist_values,
+                                   sep = "&",
+                                   ggObj = ggObj,
+                                   dataFrame = dataFrame,
+                                   column_names = column_names,
+                                   facet_id = facet_id)
+
+  fill_var <- operations$fill_var
+  hist_values <- operations$hist_values
+
+  if(!is.null(ggObj$labels$fill) && ggObj$labels$fill != ggObj$labels$x && is(position, "PositionStack")) {
 
     y <- hist_data$y
     levels_bar <- levels(factor(hist_values))
-
-    fill_var <- unlist(
-      dataFrame[, which(stringr::str_detect(ggObj$labels$fill, column_names) == TRUE)]
-    )
-    panel_i.fill_var <- fill_var[facet_id]
 
     # ylim is set some y is dropped
     if(any(is.na(y))) {
@@ -245,20 +253,23 @@ catch_bin_info.StatCount <- function(hist_values, hist_data, x.limits, y.limits,
       drops <- levels_bar[hist_data$x[na_y]]
       drop_count <- hist_data$count[na_y]
 
-      group_by_table <- data.frame(fill = panel_i.fill_var, hist_values = hist_values) %>%
+      group_by_table <- data.frame(fill = fill_var, hist_values = hist_values) %>%
         dplyr::group_by(fill, hist_values) %>%
         dplyr::summarise(n = dplyr::n())
 
       drop_fill <- group_by_table$fill[(group_by_table$hist_values %in% drops) &
                                          (group_by_table$n %in% drop_count)]
 
-      in_y.limits <- !((hist_values %in% drops) & (panel_i.fill_var %in% drop_fill))
+      in_y.limits <- !((hist_values %in% drops) & (fill_var %in% drop_fill))
     }
 
     in_limits <- in_y.limits & in_x.limits
     hist_values <- hist_values[in_limits]
-    panel_i.fill_var <- panel_i.fill_var[in_limits]
-    fill_levels <- rev(levels(as.factor(panel_i.fill_var)))
+    fill_var <- fill_var[in_limits]
+    fill_levels <- levels(as.factor(fill_var))
+    if(is.null(reverse) || !reverse) {
+      fill_levels <- rev(fill_levels)
+    }
 
   } else {
 
@@ -271,8 +282,10 @@ catch_bin_info.StatCount <- function(hist_values, hist_data, x.limits, y.limits,
     }
     in_limits <- in_y.limits & in_x.limits
     hist_values <- hist_values[in_limits]
-    panel_i.fill_var <- hist_values
-    fill_levels <- levels(as.factor(panel_i.fill_var))
+    fill_levels <- levels(as.factor(fill_var))
+    if(!is.null(reverse) && reverse) {
+      fill_levels <- rev(fill_levels)
+    }
   }
 
   uni_fill <- hex6to12(unique(hist_data$fill))
@@ -281,7 +294,7 @@ catch_bin_info.StatCount <- function(hist_values, hist_data, x.limits, y.limits,
 
     fill <- rep(NA, length(hist_values))
     for(j in seq_len(length(fill_levels))){
-      fill[which(panel_i.fill_var %in% fill_levels[j])] <- uni_fill[j]
+      fill[which(fill_var %in% fill_levels[j])] <- uni_fill[j]
     }
   } else {
     fill <- rep(uni_fill, length(hist_values))
@@ -294,6 +307,78 @@ catch_bin_info.StatCount <- function(hist_values, hist_data, x.limits, y.limits,
     hist_values = hist_values,
     colorStackingOrder = colorStackingOrder,
     in_limits = in_limits
+  )
+}
+
+# bar position operation
+# PositionStack is stack the colored bins
+# PositionDodge is place the stacked bar side by side
+position_operation <- function(position, hist_values, sep = "&", ...) {
+  UseMethod("position_operation", position)
+}
+position_operation.PositionStack <- function(position, hist_values, sep = "&", ...) {
+  args <- list(...)
+  ggObj <- args$ggObj
+  dataFrame <- args$dataFrame
+  column_names <- args$column_names
+  facet_id <- args$facet_id
+
+  if(!is.null(ggObj$labels$fill) && ggObj$labels$fill != ggObj$labels$x) {
+
+    fill_var <- unlist(
+      dataFrame[, which(stringr::str_detect(ggObj$labels$fill, column_names) == TRUE)]
+    )[facet_id]
+
+  } else {
+    fill_var <- hist_values
+  }
+
+  list(
+    hist_values = hist_values,
+    fill_var = fill_var
+  )
+}
+
+position_operation.PositionDodge <- function(position, hist_values, sep = "&", ...) {
+
+  args <- list(...)
+  ggObj <- args$ggObj
+  dataFrame <- args$dataFrame
+  column_names <- args$column_names
+  facet_id <- args$facet_id
+
+  if(!is.null(ggObj$labels$fill) && ggObj$labels$fill != ggObj$labels$x) {
+
+    fill_var <- unlist(
+      dataFrame[, which(stringr::str_detect(ggObj$labels$fill, column_names) == TRUE)]
+    )[facet_id]
+
+    if(length(hist_values) == length(fill_var)) {
+
+      # TODO Class PositionDodge2 (with reverse = TRUE)
+      # reverse <- position$reverse
+      # if(!is.null(reverse) && reverse) {
+      #
+      #   # reorder levels
+      #   hist_values_levels <- c(t(outer(levels(factor(hist_values)), rev(levels(factor(fill_var))), paste, sep = sep)))
+      #   hist_values <- factor(paste(hist_values, fill_var, sep = sep))
+      #   levels(hist_values) <- hist_values_levels[hist_values_levels %in% unique(hist_values)]
+      #
+      # } else {
+      #   hist_values <- paste(hist_values, fill_var, sep = sep)
+      # }
+      hist_values <- paste(hist_values, fill_var, sep = sep)
+      message("Viewport is changed. Set `l_scaleto_world` to get the world view")
+    } else
+      warning("The length does not match. It should not happen.", call. = FALSE)
+
+  } else {
+    fill_var <- hist_values
+  }
+
+  list(
+    hist_values = hist_values,
+    fill_var = fill_var
   )
 }
 
