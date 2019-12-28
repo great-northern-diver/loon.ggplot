6c0ed90c91fde8c67fb9c87fe47217f115fdaeb6
z267xu <z267xu@github.com>
2019-12-27 20:55:27 -0500

    generate defualt ggplot colors

---
+++ R/gg_color_hue.R
@@ -0,0 +1,8 @@
+#' @title Default \code{ggplot} color palette
+#' @description Generate default discrete \code{ggplot} color
+#' @param n the number of colors
+#' @export
+gg_color_hue <- function(n) {
+  hues <- seq(15, 375, length = n + 1)
+  grDevices::hcl(h = hues, l = 65, c = 100)[1:n]
+}
