04a393e0216321650dc6ac950a46c3eacce44595
z267xu <z267xu@github.com>
2020-03-21 17:56:50 -0400

    unexported loon functions

---
+++ R/unexportedLoonFuns.R
@@ -0,0 +1,20 @@
+################################ Unexported functions in `loon` but used in `loon.ggplot` ################################
+# Since `Unexported objects imported by ':::' calls` will cause a NOTE in R CMD check
+
+## Unexported functions in loon
+as_r_polygonGlyph_size <- getFromNamespace("as_r_polygonGlyph_size", "loon")
+get_scaledData <- getFromNamespace("get_scaledData", "loon")
+as_r_serialaxesGlyph_size <- getFromNamespace("as_r_serialaxesGlyph_size", "loon")
+glyph_to_pch <- getFromNamespace("glyph_to_pch", "loon")
+get_display_color <- getFromNamespace("get_display_color", "loon")
+as_hex6color <- getFromNamespace("as_hex6color", "loon")
+get_font_info_from_tk <- getFromNamespace("get_font_info_from_tk", "loon")
+xy_coords_layer <- getFromNamespace("xy_coords_layer", "loon")
+get_layer_states <- getFromNamespace("get_layer_states", "loon")
+getBinData <- getFromNamespace("getBinData", "loon")
+get_layer_states <- getFromNamespace("get_layer_states", "loon")
+get_model_display_order <- getFromNamespace("get_model_display_order", "loon")
+char2num.data.frame <- getFromNamespace("char2num.data.frame", "loon")
+cartesian_model_widget_states <- getFromNamespace("cartesian_model_widget_states", "loon")
+tcl_img_2_r_raster <- getFromNamespace("tcl_img_2_r_raster", "loon")
+l_toplevel <- getFromNamespace("l_toplevel", "loon")
