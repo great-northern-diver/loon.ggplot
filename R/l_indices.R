874bbe490b53863d32cb30872369b27f0b24204a
z267xu <z267xu@github.com>
2020-08-20 14:55:25 -0400

    To each panel, return the indices

--- R/l_indices.R
+++ R/l_indices.R
@@ -1,3 +1,68 @@
+l_indices <- function(ggObj, panelIndex, args,
+                      plotInfo, numOfSubtitles, modelLayers, activeInfo) {
+
+  # grab objects from the input
+  isCoordSerialaxes <- plotInfo$isCoordSerialaxes
+  buildggObj <- plotInfo$buildggObj
+
+  # gg build
+  ggplotPanel_params <- buildggObj$ggplotPanel_params
+  ggBuild <- buildggObj$ggBuild
+  layout <- buildggObj$layout
+
+  lenLayers <- length(ggObj$layers)
+  isCoordPolar <- is.CoordPolar(ggplotPanel_params[[panelIndex]])
+  dataFrame <- ggObj$data
+
+  activeGeomLayers <- activeInfo$activeGeomLayers
+  activeModel <- activeInfo$activeModel
+
+  # boxplot has a hidden scatterplot model layer
+  boxplot_point_layers <- c(modelLayers$boxplotLayers, activeGeomLayers)
+
+  if(activeModel == "l_serialaxes" & length(activeGeomLayers) > 0) {
+
+    l_serialaxes_indices(ggBuild = ggBuild,
+                         activeGeomLayers = activeGeomLayers,
+                         panelIndex = panelIndex,
+                         ggObj = ggObj)
+
+  } else if (activeModel == "l_hist" & length(activeGeomLayers) > 0) {
+
+    is_facet_wrap <- plotInfo$is_facet_wrap
+    is_facet_grid <- plotInfo$is_facet_grid
+    ggLayout <- buildggObj$ggLayout
+
+    if (is.data.frame(dataFrame) & !is.waive(dataFrame)) {
+      mapping <- ggObj$mapping
+    } else {
+      if(length(activeGeomLayers) == 1) {
+        dataFrame <- ggObj$layers[[activeGeomLayers]]$data
+        mapping <- ggObj$layers[[activeGeomLayers]]$mapping
+      } else mapping <- ggplot2::aes() # activeGeomLayers > 1 not implemented so far
+    }
+
+    l_hist_indices(ggBuild = ggBuild,
+                   activeGeomLayers = activeGeomLayers,
+                   panelIndex = panelIndex,
+                   mapping = mapping,
+                   dataFrame = dataFrame,
+                   numOfSubtitles = numOfSubtitles,
+                   is_facet_wrap = is_facet_wrap,
+                   is_facet_grid = is_facet_grid,
+                   layout = layout,
+                   ggLayout = ggLayout)
+
+
+  } else if(activeModel == "l_plot" & length(boxplot_point_layers) > 0) {
+
+    l_plot_indices(ggBuild = ggBuild,
+                   activeGeomLayers = activeGeomLayers,
+                   panelIndex = panelIndex)
+
+  } else NULL
+}
+
 l_plot_indices <- function(ggBuild, activeGeomLayers, panelIndex) {
   unlist(
     lapply(activeGeomLayers,
@@ -9,10 +74,27 @@ l_plot_indices <- function(ggBuild, activeGeomLayers, panelIndex) {
   )
 }
 
+l_serialaxes_indices <- function(ggBuild, activeGeomLayers, panelIndex, ggObj) {
+
+  # The warning is given ahead; See function `loon.ggplot:::get_modelLayers`
+  activeGeomLayers <- activeGeomLayers[1]
+  indices <- which(ggBuild$data[[activeGeomLayers]]$PANEL == panelIndex)
+  aesData <- ggBuild$data[[activeGeomLayers]][indices, ]
+
+  layer <- ggObj$layers[[activeGeomLayers]]
+  if(inherits(layer$geom, "GeomRibbon") || inherits(layer$geom, "GeomPath")) {
+
+    # Each line is repeated by the same times
+    offset <- sum(aesData$group == aesData$group[1])
+    unique(ceiling(indices/offset))
+
+  } else NULL
+}
+
 # it is a wrapper of the function `catch_facet_id`
 l_hist_indices <- function(ggBuild, activeGeomLayers, panelIndex, mapping, dataFrame,
-                               numOfSubtitles, is_facet_wrap, is_facet_grid,
-                               layout, ggLayout) {
+                           numOfSubtitles, is_facet_wrap, is_facet_grid,
+                           layout, ggLayout) {
 
   hist_data <- ggBuild$data[[activeGeomLayers]]
 
