fe10d06682722ac80c54e1b1c22d690fac1a1cc0
z267xu <z267xu@github.com>
2020-08-20 23:22:30 -0400

    new tests (serialaxes coord)

--- tests/testthat/test_ggplot2loon_serialAxesCoord.R
+++ tests/testthat/test_ggplot2loon_serialAxesCoord.R
@@ -3,55 +3,77 @@ library(dplyr)
 library(magrittr)
 library(tidyverse)
 library(GGally)
-library(png)
 
-test_that("test serialaxes ggplot", {
+test_that("test serialaxes ggplot",
+          {
+            # histogram
+            ### parallel
+            ggplot(iris) +
+              geom_path(alpha = 0.2) +
+              geom_histogram(alpha = 0.8,
+                             mapping = aes(fill = factor(Species))) +
+              coord_serialaxes() -> p
+            x <- ggplot_build(p)
+            expect_equal(length(x$plot$layers), 2)
+            expect_true("CoordSerialaxes" %in% class(p$coordinates))
+            expect_false("CoordSerialaxes" %in% class(x$coordinates))
 
-  # histogram
-  ggplot(iris) +
-    geom_path(alpha = 0.2) +
-    geom_histogram(alpha = 0.8,
-                   mapping = aes(fill = factor(Species))) +
-    coord_serialaxes() -> p
-  x <- ggplot_build(p)
-  expect_equal(length(x$plot$layers), 2)
-  expect_true("CoordSerialaxes" %in% class(p$coordinates))
-  expect_false("CoordSerialaxes" %in% class(x$coordinates))
+            ### radial
+            p$coordinates$axesLayout <- "radial"
+            x <- ggplot_build(p)
+            expect_false("CoordSerialaxes" %in% class(x$coordinates))
 
-  # freqpoly
-  suppressWarnings(
-    ggplot(iris) +
-      geom_path(alpha = 0.2) +
-      geom_freqpoly(alpha = 0.8,
-                    mapping = aes(fill = factor(Species))) +
-      coord_serialaxes() -> p
-  )
-  x <- ggplot_build(p)
-  expect_equal(length(x$plot$layers), 2)
+            # freqpoly
+            ### parallel
+            suppressWarnings(
+              ggplot(iris) +
+                geom_path(alpha = 0.2) +
+                geom_freqpoly(alpha = 0.8,
+                              mapping = aes(fill = factor(Species))) +
+                coord_serialaxes() -> p
+            )
+            x <- ggplot_build(p)
+            expect_equal(length(x$plot$layers), 2)
 
-  # density
-  ggplot(mtcars) +
-    geom_path(alpha = 0.2) +
-    geom_density(alpha = 0.8,
-                 mapping = aes(fill = factor(cyl))) +
-    coord_serialaxes(axesLayout = "radial") -> p
-  x <- ggplot_build(p)
-  expect_equal(length(x$plot$layers), 2)
-  expect_true("CoordSerialaxes" %in% class(p$coordinates))
-  expect_false("CoordSerialaxes" %in% class(x$coordinates))
-})
+            ### radial
+            p$coordinates$axesLayout <- "radial"
+            x <- ggplot_build(p)
+            expect_false("CoordSerialaxes" %in% class(x$coordinates))
 
-test_that("test serialaxes ggplot 2 loon", {
+            # density
+            ### parallel
+            ggplot(mtcars) +
+              geom_path(alpha = 0.2, mapping = aes(colour = factor(cyl))) +
+              geom_density(alpha = 0.8,
+                           mapping = aes(fill = factor(cyl))) +
+              coord_serialaxes() -> p
+            x <- ggplot_build(p)
+            expect_true("CoordSerialaxes" %in% class(p$coordinates))
+            expect_false("CoordSerialaxes" %in% class(x$coordinates))
 
-  l_ggplot(iris, mapping = aes(colour = factor(Species))) +
-    geom_path(alpha = 0.2) +
-    coord_serialaxes() -> p
-  q <- loon.ggplot(p)
-  expect_true("l_serialaxes" %in% class(q))
-  expect_equal(q['axesLayout'], "parallel")
+            ### radial
+            p$coordinates$axesLayout <- "radial"
+            x <- ggplot_build(p)
+            expect_false("CoordSerialaxes" %in% class(x$coordinates))
+          })
 
-  p$coordinates$axesLayout <- "radial"
-  q <- loon.ggplot(p)
-  expect_true("l_serialaxes" %in% class(q))
-  expect_equal(q['axesLayout'], "radial")
-})
+test_that("test serialaxes ggplot 2 loon",
+          {
+            l_ggplot(iris, mapping = aes(colour = factor(Species))) +
+              geom_path(alpha = 0.2) +
+              coord_serialaxes() -> p
+            q <- loon.ggplot(p)
+            expect_true("l_serialaxes" %in% class(q))
+            expect_equal(q['axesLayout'], "parallel")
+
+            p$coordinates$axesLayout <- "radial"
+            q <- loon.ggplot(p)
+            expect_true("l_serialaxes" %in% class(q))
+            expect_equal(q['axesLayout'], "radial")
+
+            l_ggplot(iris, mapping = aes(colour = factor(Species))) +
+              geom_ribbon(alpha = 0.2) +
+              coord_serialaxes() -> p
+            q <- loon.ggplot(p)
+            expect_true(q['showArea'])
+          })
