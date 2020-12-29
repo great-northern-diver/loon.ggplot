85aa1bfd4cd271127968e9f7f27e29b1fae9725b
z267xu <z267xu@github.com>
2020-12-28 21:19:22 -0500

    rlang::warn, rlang::abort stringr::str_detect to warning, stop and grepl correspondingly.

--- R/deprecated_functions.R
+++ R/deprecated_functions.R
@@ -1,12 +1,17 @@
+#' @title Add image glyphs on scatter plot
+#' @description Each point glyph can be a image (png, jpeg, etc) object.
 #' @export
-#' @inherit ggmulti::geom_image_glyph
+#' @inheritParams ggmulti::geom_image_glyph
+#' @param width Numerical; width of image
+#' @param height Numerical; height of image
 geom_imageGlyph <- function(mapping = NULL, data = NULL, stat = 'identity',
                             position = 'identity', ...,
                             images, width = 4, height = 3, na.rm = FALSE,
                             show.legend = NA,
                             inherit.aes = TRUE) {
-  rlang::warn(
-    "Function `geom_imageGlyph` is deprecated, please use function `geom_image_glyph` in package 'ggmulti'"
+  warning(
+    "Function `geom_imageGlyph` is deprecated, ",
+    "please use function `geom_image_glyph` in package 'ggmulti'", call. = FALSE
   )
 
   if(missing(images)) {
@@ -22,15 +27,19 @@ geom_imageGlyph <- function(mapping = NULL, data = NULL, stat = 'identity',
   )
 }
 
+#' @title Add polygon glyphs on scatter plot
+#' @description Each point glyph can be a polygon object. Available polygons coords can be achieved in \code{\link{polygon_glyph}}
 #' @export
-#' @inherit ggmulti::geom_polygon_glyph
+#' @inheritParams ggmulti::geom_polygon_glyph
+#' @param showArea show area; deprecated now, please set `fill` or `colour` to control the shown area.
 geom_polygonGlyph <- function(mapping = NULL, data = NULL, stat = 'identity',
                               position = 'identity', ...,
                               polygon_x, polygon_y, showArea = TRUE, linewidth = 1,
                               na.rm = FALSE, show.legend = NA,
                               inherit.aes = TRUE) {
-  rlang::warn(
-    "Function `geom_polygonGlyph` is deprecated, please use function `geom_polygon_glyph` in package 'ggmulti'"
+  warning(
+    "Function `geom_polygonGlyph` is deprecated, ",
+    "please use function `geom_polygon_glyph` in package 'ggmulti'", call. = FALSE
   )
 
   if(missing(polygon_x) || missing(polygon_y)) {
@@ -48,8 +57,18 @@ geom_polygonGlyph <- function(mapping = NULL, data = NULL, stat = 'identity',
   )
 }
 
+#' @title Add serialaxes glyphs on scatter plot
+#' @description To visualize high dimensional data on scatterplot. Each point glyph is surrounded by a serialaxes object.
 #' @export
-#' @inherit ggmulti::geom_serialaxes_glyph
+#' @inheritParams ggmulti::geom_serialaxes_glyph
+#' @param serialAxesData a serial axes numerical data set. If not provided, `geom_point()` will be called.
+#' @param sequence A vector with variable names that defines the axes sequence
+#' @param axesLayout either "radial" or "parallel"
+#' @param showAxes boolean to indicate whether axes should be shown or not
+#' @param showArea show area; deprecated now, please set `fill` or `colour` to control the shown area.
+#' @param showEnclosing boolean to indicate whether enclosing should be shown or not
+#' @param axesColor axes color
+#' @param bboxColor bounding box color
 geom_serialAxesGlyph <- function(mapping = NULL, data = NULL, stat = 'identity',
                                  position = 'identity', ...,
                                  serialAxesData, sequence = NULL, linewidth = 1,
@@ -58,8 +77,10 @@ geom_serialAxesGlyph <- function(mapping = NULL, data = NULL, stat = 'identity',
                                  showAxes = FALSE, showArea = FALSE,  showEnclosing = FALSE,
                                  axesColor = "black", bboxColor = 'black',na.rm = FALSE, show.legend = NA,
                                  inherit.aes = TRUE) {
-  rlang::warn(
-    "Function `geom_polygonGlyph` is deprecated, please use function `geom_polygon_glyph` in package 'ggmulti'"
+  warning(
+    "Function `geom_polygonGlyph` is deprecated, ",
+    "please use function `geom_polygon_glyph` in package 'ggmulti'",
+    call. = FALSE
   )
 
   if(missing(serialAxesData)) {
@@ -80,16 +101,21 @@ geom_serialAxesGlyph <- function(mapping = NULL, data = NULL, stat = 'identity',
   )
 }
 
+#' @title Pointrange
 #' @export
-#' @inherit ggplot2::geom_pointrange
+#' @inheritParams ggplot2::geom_pointrange
+#' @param ymin y min
+#' @param ymax y max
+#' @param showArea show area; deprecated now, please set `fill` or `colour` to control the shown area.
+#' @param linewidth line width
 geom_pointrangeGlyph <- function(mapping = NULL, data = NULL, stat = 'identity',
                                  position = 'identity', ...,
                                  ymin, ymax, showArea = TRUE, linewidth = 1,
                                  na.rm = FALSE, show.legend = NA,
                                  inherit.aes = TRUE) {
-  rlang::warn(
-    "Function `geom_pointrangeGlyph` is deprecated,
-    please use function `ggplot2::geom_pointrange` instead."
+  warning(
+    "Function `geom_pointrangeGlyph` is deprecated, ",
+    "please use function `ggplot2::geom_pointrange` instead.", call. = FALSE
   )
 
   ggplot2::geom_pointrange(
@@ -104,16 +130,16 @@ geom_pointrangeGlyph <- function(mapping = NULL, data = NULL, stat = 'identity',
   )
 }
 
+#' @title Text
 #' @export
-#' @inherit ggplot2::geom_text
+#' @inheritParams ggplot2::geom_text
+#' @param text The test to display
 geom_textGlyph <- function(mapping = NULL, data = NULL, stat = 'identity',
                            position = 'identity', ..., text,
                            na.rm = FALSE, show.legend = NA,
                            inherit.aes = TRUE) {
-  rlang::warn(
-    "Function `geom_textGlyph` is deprecated,
-    please use function `ggplot2::geom_text` instead."
-  )
+  warning(
+    "Function `geom_textGlyph` is deprecated, please use function `ggplot2::geom_text` instead.", call. = FALSE)
 
   ggplot2::geom_text(
     mapping = mapping,
