92489e98ddd92d25f912aa9981690adfcccf46eb
z267xu <z267xu@github.com>
2020-12-28 21:20:35 -0500

    rlang::warn, rlang::abort stringr::str_detect to warning, stop and grepl correspondingly.

--- R/interactive-ggproto.R
+++ R/interactive-ggproto.R
@@ -1,4 +1,4 @@
-#' @title Modify the interactive component
+#' @title Modify the \code{interactivity} component
 #' @description Set interactive components (e.g. \code{linking}, \code{selecting}, \code{itemLabel}, etc)
 #' @inheritParams linking
 #' @inheritParams selecting
@@ -63,16 +63,18 @@ interactivity <- function(linkingGroup = NULL,
                    warn_nDim_states = function(data, x) {
                      n <- nrow(data) %||% 0
                      if(length(x) != n && length(x) != 1) {
-                       warning("The length of ", deparse(substitute(x)), " is ", length(x),
-                               " that does not match the number of observations(",
-                               n, ")",
-                               call. = FALSE)
+                       warning(
+                         "The length of ", deparse(substitute(x)),
+                         " is ", length(x),
+                         " that does not match the number of observations(" ,
+                         n, ")."
+                       )
                      }
                    },
                    check_itemLabel = function(self, data, params) {
 
                      if(!is.null(params$showItemLabels) && !is.logical(params$showItemLabels)) {
-                       rlang::abort("`showItemLabels` must be logical")
+                       stop("`showItemLabels` must be logical", call. = FALSE)
                      }
 
                      itemLabel <- params$itemLabel
@@ -88,7 +90,7 @@ interactivity <- function(linkingGroup = NULL,
                      if(is.null(selected)) return(NULL)
 
                      if(!is.logical(selected)) {
-                       rlang::abort("`selected` must be logical")
+                       stop("`selected` must be logical", call. = FALSE)
                      }
 
                      self$warn_nDim_states(data = data,
@@ -100,7 +102,7 @@ interactivity <- function(linkingGroup = NULL,
                      if(is.null(linkingKey)) return(NULL)
 
                      if(any(duplicated(linkingKey)))
-                       rlang::abort("The linkingKey is duplicated")
+                       stop("The linkingKey is duplicated", call. = FALSE)
 
                      self$warn_nDim_states(data = data,
                                            x = linkingKey)
