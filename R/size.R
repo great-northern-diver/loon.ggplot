88507a804db5181aa25aef19bbb5e46b1d643f67
Zehao Xu <z267xu@uwaterloo.ca>
2021-07-15 15:37:40 -0400

    move arg `adjust` into px2pt() and pt2px()

--- R/size.R
+++ R/size.R
@@ -3,6 +3,7 @@ as_ggplot_size <- function(size,
                            type = c("points", "texts", "images",
                                     "radial", "parallel", "polygon",
                                     "lines"),
+                           adjust = 1, # A HACK
                            ...) {
 
   type <- match.arg(type)
@@ -31,9 +32,9 @@ as_ggplot_size <- function(size,
       # pixel(unit)
       diameter.px <- sqrt(area/pi) * 2
       # pixel(unit) to pt(unit)
-      diameter.pt <- diameter.px * 1/pt2px()
+      diameter.pt <- diameter.px * px2pt(adjust = adjust)
       # formula which is defined by `ggplot2::GeomPoints`
-      (diameter.pt - ggplot2::GeomPoint$default_aes$stroke * ggplot2::.stroke / 2)/ggplot2::.pt
+      (diameter.pt - ggplot2::GeomPoint$default_aes$stroke * ggplot2::.stroke/2)/ggplot2::.pt
     },
     texts = {
       ## Text Glyph
@@ -41,7 +42,7 @@ as_ggplot_size <- function(size,
       ## size >= 1 --> 2 + size (area in pixel)
       area <- ifelse(size < 1, 2, 2 + size)
       # pixel(unit) to pt(unit)
-      area.pt <- area * 1/pt2px()
+      area.pt <- area * px2pt(adjust = adjust)
       # formula which is defined by `ggplot2::GeomText`
       (area.pt)/ggplot2::.pt
     },
@@ -56,18 +57,18 @@ as_ggplot_size <- function(size,
       # height
       height.px <- sqrt(area * ratio)
       # output unit is cm
-      height.px/cm2px()
+      height.px * px2cm(adjust = adjust)
     },
     polygon = {
       # output unit is cm
-      ifelse(size < 1, 4, 6 * sqrt(size)) / cm2px()
+      ifelse(size < 1, 4, 6 * sqrt(size)) * px2cm(adjust = adjust)
     },
     radial = {
       area <- ifelse(size < 1, 25, 400 * size)
       # pixel(unit)
       diameter.px <- sqrt(area/pi) * 2
       # output unit is cm
-      diameter.px / cm2px()
+      diameter.px * px2cm(adjust = adjust)
     },
     parallel = {
       args <- list(...)
@@ -76,17 +77,23 @@ as_ggplot_size <- function(size,
       area <- ifelse(size < 1, 9 * (p - 1), 64 * (p - 1) * size)
       # height:width = 1:2
       # return height
-      sqrt(area/2) / cm2px()
+      sqrt(area/2) * px2cm(adjust = adjust)
     },
     lines = {
-      size/(pt2px() * ggplot2::.pt)
+      # output unit is mm
+      # suppose the unit of loon is in px
+      # size/(cm2px()/10 * ggplot2::.pt)
+      # suppose the unit of loon is in mm
+      size/ggplot2::.pt * px2pt(adjust = adjust)
     }
   )
 }
 
-as_loon_size <- function(size, type = c("points", "texts", "images",
-                                        "radial", "parallel", "polygon",
-                                        "lines"),
+as_loon_size <- function(size,
+                         type = c("points", "texts", "images",
+                                  "radial", "parallel", "polygon",
+                                  "lines"),
+                         adjust = 1,
                          ...) {
 
   type <- match.arg(type)
@@ -107,18 +114,24 @@ as_loon_size <- function(size, type = c("points", "texts", "images",
            ## size < 1 --> 8 (area in pixel)
            ## size >= 1 --> 12 * size (area in pixel)
            diameter.pt <- size * .pt + stroke  * .stroke / 2
-           diameter.px <- diameter.pt * pt2px()
+           diameter.px <- diameter.pt * pt2px(adjust = adjust)
            area <- (diameter.px/2)^2 * pi
            ifelse(area < 8, 1, area/12)
          },
-         lines = (pt2px() * ggplot2::.pt) * size,
+         lines = {
+           # output unit is mm
+           # suppose the unit of loon is in px
+           # (cm2px()/10 * ggplot2::.pt) * size
+           # suppose the unit of loon is in mm
+           size * ggplot2::.pt * pt2px(adjust = adjust)
+         },
          texts = {
            ## Text Glyph
            ## size < 1 --> 2 (area in pixel)
            ## size >= 1 --> 2 + size (area in pixel)
            area.pt <- size * .pt
-           area <- area.pt * pt2px()
-           ifelse(area < 2, 1, area - 2)
+           area <- area.pt * pt2px(adjust = adjust)
+           ifelse(area < 2, 1, (area - 2))
          },
          images = {
            args <- list(...)
@@ -129,18 +142,18 @@ as_loon_size <- function(size, type = c("points", "texts", "images",
            ## size >= 1 --> 600 * size (area in pixel)
 
            # size is height
-           height.px <- size * cm2px()
+           height.px <- size * cm2px(adjust = adjust)
            area <- height.px^2/ratio
            ifelse(area < 20, 1, area/600)
          },
          polygon = {
            # unit is cm
            # to px
-           size <- size * cm2px()
+           size <- size * cm2px(adjust = adjust)
            ifelse(size < 4, 1, (size/6)^2)
          },
          radial = {
-           diameter.px <- size * cm2px()
+           diameter.px <- size * cm2px(adjust = adjust)
            area <- (diameter.px/2)^2 * pi
            ifelse(area < 25, 1, area/400)
          },
@@ -148,14 +161,21 @@ as_loon_size <- function(size, type = c("points", "texts", "images",
            args <- list(...)
            # ratio = height/width
            p <- args$p
-           area <- (size * cm2px())^2 * 2
+           area <- (size * cm2px(adjust = adjust))^2 * 2
            ifelse(area < 9 * (p - 1), 1, area/(64 * (p - 1)))
          }
   )
 }
 
-pt2px <- function() 1.3281472327365
-cm2px <- function() 37.7952755906
-
-# THIS IS A HACK
-size_adjust <- function() 0.6
+pt2px <- function(adjust = 1) 4/3 * adjust
+px2pt <- function(adjust = 1) 3/4 * adjust
+cm2px <- function(adjust = 1) {
+  # grid::convertUnit(grid::unit(1, "cm"), "pt",
+  #                   valueOnly = TRUE) * pt2px(adjust = adjust)
+  .pt * 10 * pt2px(adjust = adjust)
+}
+px2cm <- function(adjust = 1) {
+  # grid::convertUnit(grid::unit(1, "pt"), "cm",
+  #                   valueOnly = TRUE) * px2pt(adjust = adjust)
+  px2pt(adjust = adjust)/(.pt * 10)
+}
