12ecc6a33d49fb9b7f02148bfbf72e676bebe644
Zehao Xu <z267xu@uwaterloo.ca>
2021-09-04 11:47:49 -0400

    rewrite compound function (by `patchwork` rule)

---
+++ R/loon2ggplot-l_ts.R
@@ -0,0 +1,17 @@
+# deprecated
+# #' @rdname loon2ggplot
+# #' @export
+# loon2ggplot.l_ts <- function(target, asAes = TRUE, selectedOnTop = TRUE,
+#                              showNearestColor = FALSE, ...) {
+#
+#   # locations <- g_getLocations(target)
+#   #
+#   # GGally::ggmatrix(plots = g_getPlots(target, asAes = asAes,
+#   #                                     selectedOnTop = selectedOnTop,
+#   #                                     showNearestColor = showNearestColor),
+#   #                  title = loon_title(target),
+#   #                  showYAxisPlotLabels = TRUE,
+#   #                  yAxisLabels = names(target),
+#   #                  nrow = locations$nrow,
+#   #                  ncol = locations$ncol)
+# }
