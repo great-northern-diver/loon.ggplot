22c67ebff1f300a0c5dca3a16d99850df5ca9d13
Zehao Xu <z267xu@uwaterloo.ca>
2022-01-20 18:11:43 -0500

    use the function `facet_grid()`

--- R/loon2ggplot-l_facet_grid.R
+++ R/loon2ggplot-l_facet_grid.R
@@ -3,13 +3,82 @@
 loon2ggplot.l_facet_grid <- function(target, asAes = TRUE, selectedOnTop = TRUE,
                                      showNearestColor = FALSE, ...) {
 
+  tryCatch(
+    expr = {
+
+      labels <- l_facet_grid_getLabels(target)
+      facetsLabels <- labels$facetsLabels
+      levels <- labels$levels
+      # widgets in a loon facet object can have different layers
+      # (after creating an l_facet object,
+      # people can still modify each of them individually),
+      # however, ggplot2 cannot. Therefore, layers in the first plot in
+      # the facet will be referred.
+      n <- length(target)
+      lp <- loon2ggplot(target[[1L]],
+                        asAes = asAes, selectedOnTop = selectedOnTop,
+                        showNearestColor = showNearestColor,
+                        facets = target,
+                        facetsLabels = facetsLabels,
+                        levels = levels, ...)
+
+      lp$labels$x <- labels$xlabel
+      lp$labels$y <- labels$ylabel
+      lp$labels$title <- labels$title
+
+      facetsColLabels <- labels$facetsColLabels
+      facetsRowLabels <- labels$facetsRowLabels
+
+      cols <- rownames(facetsColLabels)
+      rows <- rownames(facetsRowLabels)
+
+      if(length(rows) == 0 && length(cols) > 0) {
+        formula <- as.formula(paste0("~", paste(cols, collapse = "+")))
+      } else if (length(rows) > 0 && length(cols) == 0) {
+        formula <- as.formula(paste0(paste(rows, collapse = "+"), "~."))
+      } else if (length(rows) > 0 && length(cols) > 0) {
+        formula <- as.formula(paste0(paste(rows, collapse = "+"), "~",
+                                     paste(cols, collapse = "+")))
+      } else stop("No `rows` and `cols`", call. = FALSE)
+
+      labelsLocation <- labels$labelsLocation
+      if (labelsLocation[1] == "top" && labelsLocation[2] == "right") {
+        switch <- NULL
+      } else if (labelsLocation[1] == "top" && labelsLocation[2] == "left") {
+        switch <- "y"
+      } else if (labelsLocation[1] == "bottom" && labelsLocation[2] == "right") {
+        switch <- "x"
+      } else {
+        # "bottom", "left"
+        switch <- "both"
+      }
+
+      lp +
+        ggplot2::facet_grid(formula,
+                            switch = switch,
+                            drop = FALSE)
+    },
+    error = function(e) {
+
+      warning(e$message,
+              ". The plots will be constructed by `patchwork`.", call. = FALSE)
+
+      patchwork_facet_grid(target, asAes = asAes, selectedOnTop = selectedOnTop,
+                           showNearestColor = showNearestColor, ...)
+    }
+  )
+}
+
+patchwork_facet_grid <- function(target, asAes = TRUE, selectedOnTop = TRUE,
+                                 showNearestColor = FALSE, ...) {
+
   args <- loon::l_get_arrangeGrobArgs(target)
   # they are loonGrobs
   grobs <- args$grobs
   grobsNames <- vapply(grobs, function(grob) grob$name, character(1L))
 
   ggplots <- loon2ggplot.l_compound(target, asAes, selectedOnTop,
-                                    showNearestColor)
+                                    showNearestColor, ...)
 
   # drop guides
   ggplots <- ggplots &
@@ -51,7 +120,7 @@ loon2ggplot.l_facet_grid <- function(target, asAes = TRUE, selectedOnTop = TRUE,
                          )
                      })
   layout_matrix <- args$layout_matrix
-  positions <- layout_matrix2tlbr(layout_matrix, n = length(grobs))
+  positions <- layout_matrix2positions(layout_matrix, n = length(grobs))
 
   plots <- c(list(ggplots), ggLabels)
   plots$design <- do.call(c,
@@ -63,5 +132,4 @@ loon2ggplot.l_facet_grid <- function(target, asAes = TRUE, selectedOnTop = TRUE,
   ggCompound(plots) &
     ggplot2::labs(x = args$bottom,
                   y = args$left)
-
 }
